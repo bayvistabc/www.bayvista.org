535103673e1343037c736d3a18a13ccd801651aa
Daniel J. Summers <daniel@bitbadger.solutions>
2020-05-30 12:27:08 -0500

    Move podcast files to secure URL (#5)

--- site/themes/bayvista/static/js/play-count.js
+++ site/themes/bayvista/static/js/play-count.js
@@ -3,7 +3,7 @@ var bayvista = {
   countPlay: function (fileLink) {
     if (!this.counted) {
       var request = new XMLHttpRequest()
-      request.open('HEAD', 'https://pdcst.click/c/bay-vista/files.djs-consulting.com/sermons/' + fileLink, true)
+      request.open('HEAD', 'https://pdcst.click/c/bay-vista/files.bitbadger.solutions/sermons/' + fileLink, true)
       request.onload = function () { bayvista.counted = true }
       request.onerror = function () { }
       request.send()
